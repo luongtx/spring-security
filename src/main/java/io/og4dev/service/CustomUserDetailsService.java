be544d7004c91a0b2d3754dd247ef3b4998f39f6
Pasindu Owa Gamage <pasinduogdev@gmail.com>
2026-03-07 16:11:28 +0530

    Created JwtAuthFilter and CustomUserDetailsService

---
+++ src/main/java/io/og4dev/service/CustomUserDetailsService.java
@@ -0,0 +1,40 @@
+package io.og4dev.service;
+
+import io.og4dev.entity.UserEntity;
+import io.og4dev.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.jspecify.annotations.NullMarked;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CustomUserDetailsService implements UserDetailsService {
+    private final UserRepository repository;
+
+    @Override
+    @NullMarked
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        UserEntity entity = repository.findByUsername(username);
+        if (entity == null) throw new UsernameNotFoundException("User not found: "+username);
+        SimpleGrantedAuthority authority = new SimpleGrantedAuthority("ROLE_ADMIN");
+        List<SimpleGrantedAuthority> authorities = new ArrayList<>();
+        authorities.add(authority);
+        return new User(
+                entity.getUsername(),
+                entity.getPassword(),
+                entity.getEnabled(),
+                true,
+                true,
+                true,
+                authorities
+        );
+    }
+}
